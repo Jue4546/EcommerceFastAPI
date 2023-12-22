efe5f86d6c33bb8f09359000d0786227a967cb06
FujiokiO <123077637+fujiokio@users.noreply.github.com>
2023-12-22 14:16:47 +0800

    新增应用初始化功能，创建数据表和管理员账户
    Signed-off-by: FujiokiO <123077637+fujiokio@users.noreply.github.com>

--- app/db/base.py
+++ app/db/base.py
@@ -1,11 +1,58 @@
 """数据库连接配置：db/base.py"""
-from dotenv import load_dotenv
 import os
-import psycopg
+import string
+import secrets
+import app.models.table_model as models
+from dotenv import load_dotenv
+from sqlalchemy import create_engine, inspect
+from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 DATABASE_URL = os.getenv('POSTGRES_URL')
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-def get_db_connection():
-    return psycopg.connect(DATABASE_URL)
+def create_tables_if_not_exist():
+    models.Base.metadata.create_all(bind=engine, checkfirst=True)
+    create_admin_if_not_exists()
+
+
+def create_admin_if_not_exists():
+    from app.models.user_model import RegisterUser
+    from app.services.user_service import register_new_user
+    admin_username = 'admin'
+    alphabet = string.ascii_letters + string.digits + string.punctuation
+    while True:
+        admin_password = ''.join(secrets.choice(alphabet) for _ in range(8))
+        if (any(c.isupper() for c in admin_password)
+                and any(c.islower() for c in admin_password)
+                and any(c.isdigit() for c in admin_password)
+                and any(not c.isalnum() for c in admin_password)):
+            break
+    admin = RegisterUser(
+        username=admin_username,
+        email='admin@dbcd.oky.wiki',
+        password=admin_password,
+        hashed_password='',
+        is_admin=True,
+        is_disabled=False
+    )
+    register_new_user(admin)
+    print(f"Generated Admin Username: {admin_username}")
+    print(f"Generated Admin Password: {admin_password}")
+
+
+def tables_exist():
+    inspector = inspect(engine)
+    existing_tables = inspector.get_table_names()
+    expected_tables = models.Base.metadata.tables.keys()
+    return all(table in existing_tables for table in expected_tables)
+
+
+def admin_exist():
+    from app.db.crud.user_crud import get_user
+    admin_username = 'admin'
+    admin = get_user(admin_username)
+    return admin is not None
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from app.api.v1 import test_route, user_route
+from db.base import *
 
 app = FastAPI()
 
@@ -10,6 +11,15 @@ app.include_router(test_route.router)  # 添加测试模块的路由，用于判
 
 
 @app.get("/")
-def read_root():
+async def read_root():
     html_file_path = "assets/index.html"
     return FileResponse(html_file_path)
+
+
+@app.on_event("startup")
+async def startup_event():
+    """初始化"""
+    if not tables_exist():
+        create_tables_if_not_exist()
+    if not admin_exist():
+        create_admin_if_not_exists()
